716a18df9d87475f156a3eb93af2184a1873649f
Amarnath <j1999amar@gmail.com>
2023-07-25 15:09:48 +0530

    add vehicle repo

--- VehicleInterface/Interface/IVehicleInterface.cs
+++ VehicleInterface/Interface/IVehicleInterface.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleTypeModel;
 
 namespace VehicleInterface.Interface
 {
     public interface IVehicleInterface
     {
+        public   Task<VehicleType> AddVehicleType(VehicleType vehicleType);
+        public Task<ICollection<VehicleType>> GetAllVehicleType();
+        public Task<VehicleType> EditVehicleType(Guid id,VehicleType vehicleType);
     }
 }
